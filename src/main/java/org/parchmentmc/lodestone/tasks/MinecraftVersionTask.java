3ea1ad0c480ba787c5eff202a966488ec50097fd
SizableShrimp <sizableshrimp@sizableshrimp.me>
2021-08-10 19:44:52 -0500

    Document MinecraftVersionTask

--- src/main/java/org/parchmentmc/lodestone/tasks/MinecraftVersionTask.java
+++ src/main/java/org/parchmentmc/lodestone/tasks/MinecraftVersionTask.java
@@ -5,6 +5,10 @@ import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
 import org.parchmentmc.lodestone.LodestoneExtension;
 
+/**
+ * MinecraftVersionTask is used as a shared parent class among Lodestone tasks.
+ * Extending this class provides a Minecraft version property and default convention value for it.
+ */
 public abstract class MinecraftVersionTask extends DefaultTask {
     @Input
     public abstract Property<String> getMcVersion();
