d9ec8289ede7b5ffd65fbcbecb3f3cc485c29b2e
Marc Hermans <marc.hermans@ldtteam.com>
2021-08-18 18:06:32 +0200

    Bump with a silly comment.

--- src/main/java/org/parchmentmc/lodestone/LodestonePlugin.java
+++ src/main/java/org/parchmentmc/lodestone/LodestonePlugin.java
@@ -8,6 +8,7 @@ import org.gradle.api.Plugin;
 
 public class LodestonePlugin implements Plugin<Project> {
     public void apply(Project project) {
+        project.getLogger().lifecycle("Applying lodestone...");
         LodestoneExtension extension = project.getExtensions().create("lodestone", LodestoneExtension.class, project);
     }
 }
